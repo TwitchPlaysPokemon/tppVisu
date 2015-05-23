f4870cdeb7e10f52caa1740048d980fbdf4d3a26
hlixed <onehlixed1@yahoo.com>
2015-05-23 17:54:19 -0400

    Damage for solarbeam varies on weather

--- TppVisu/tppVisu/tables/moveFuncs.py
+++ TppVisu/tppVisu/tables/moveFuncs.py
@@ -154,6 +154,9 @@ def m_self_destruct (move, pkmn, opp, env, ovwr):
 def m_smelling_salts(move, pkmn, opp, env, ovwr):
     if opp.status == 'par':
         move.power *= 2
+def m_solarbeam(move, pkmn, opp, env, ovwr):
+    if env.weather in ["rain","sandstorm","hail"]:
+        move.power /= 2
         
 def m_sonic_boom    (move, pkmn, opp, env, ovwr):
     ovwr.damage = (20, 20)
